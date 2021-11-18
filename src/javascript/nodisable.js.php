5c55e83bd95e49a8c250abaafcb726735ddd32b5
Netkas <zixing.narrakas1996@gmail.com>
2021-11-18 11:48:10 -0500

    Removed DynamicalWeb 2.+ Files

--- src/javascript/nodisable.js.php
+++
@@ -1,7 +0,0 @@
-function getRidOffAutocomplete(){
-    var timer = window.setTimeout( function(){
-        $('#username, #password, #repeat_password').prop('disabled',false);
-        clearTimeout(timer);
-    }, 800);
-}
-getRidOffAutocomplete();
