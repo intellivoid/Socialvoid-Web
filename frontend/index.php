f3e86be6fc5af6ef6e86b5802e4c13348d9acf5c
Netkas <zixing.narrakas1996@gmail.com>
2021-11-18 14:56:17 -0500

    Corrected all asset routes

--- frontend/index.php
+++ frontend/index.php
@@ -3,4 +3,4 @@
     require('ppm');
     import('net.intellivoid.dynamical_web');
 
-    \DynamicalWeb\DynamicalWeb::exec('net.intellivoid.socialvoid_web');
+    \DynamicalWeb\DynamicalWeb::exec('net.intellivoid.socialvoid_web==latest');
